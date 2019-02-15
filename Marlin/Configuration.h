893c04bc02786a96b728c2e6b61c361ae1b0bc3a
reloxx13 <reloxx@interia.pl>
2019-02-15 19:29:36 +0100

    config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1011,7 +1011,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE //+ 15 //right next the bed
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 250
+#define Z_MAX_POS 250 - 5 //-5 tool holder/bowden
 
 /**
  * Software Endstops
