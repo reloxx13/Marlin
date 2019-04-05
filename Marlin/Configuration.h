421ef363c60a0870142f0f723f99025af177db66
reloxx13 <reloxx@interia.pl>
2019-04-05 17:50:07 +0200

    add printer statistics

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1542,7 +1542,7 @@
  *
  * View the current statistics with M78.
  */
-//#define PRINTCOUNTER
+#define PRINTCOUNTER
 
 //=============================================================================
 //============================= LCD and SD support ============================
