1f187e327a574f031cf327854e6829acda35f349
reloxx13 <reloxx@interia.pl>
2019-09-08 12:27:34 +0200

    vers

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender 3 SKR v1.4.4"
+#define CUSTOM_MACHINE_NAME "Ender 3 SKR v1.4.5"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
