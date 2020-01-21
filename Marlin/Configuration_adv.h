b04af7818e5fb2f0d250748af717169f0ef8b0ed
reloxx13 <reloxx@interia.pl>
2020-01-21 08:04:55 +0100

    config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1931,7 +1931,7 @@
 
   #if AXIS_IS_TMC(X)
     //#define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT       760        // reloxx
+    #define X_CURRENT       750        // reloxx
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
     #define X_RSENSE          0.11
@@ -1948,7 +1948,7 @@
 
   #if AXIS_IS_TMC(Y)
     //#define Y_CURRENT       800
-    #define Y_CURRENT       760        // reloxx
+    #define Y_CURRENT       750        // reloxx
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
@@ -1965,7 +1965,7 @@
 
   #if AXIS_IS_TMC(Z)
     //#define Z_CURRENT       800
-    #define Z_CURRENT       760        // reloxx
+    #define Z_CURRENT       800        // reloxx
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
@@ -1990,7 +1990,7 @@
 
   #if AXIS_IS_TMC(E0)
     //#define E0_CURRENT      800
-    #define E0_CURRENT       900        // reloxx
+    #define E0_CURRENT       750        // reloxx
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
