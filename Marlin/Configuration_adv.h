2c3ef8cc743d7a753c8f898b7a7896ef5fb66c14
reloxx13 <reloxx@interia.pl>
2020-01-24 10:13:47 +0100

    config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1994,7 +1994,7 @@
 
   #if AXIS_IS_TMC(Y)
     //#define Y_CURRENT       800
-    #define Y_CURRENT       1050        // reloxx
+    #define Y_CURRENT       960        // reloxx
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
