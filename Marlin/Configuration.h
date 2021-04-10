3e1b63fb22d727188b51bca5f01f533ba3ffb140
reloxx13 <reloxx@interia.pl>
2021-04-10 12:59:34 +0200

    version

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -133,7 +133,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 v1.21.0"
+#define CUSTOM_MACHINE_NAME "Ender-3 v1.22.0"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
