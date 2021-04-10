9311b55431887807436adc09e90a413d647a7306
reloxx13 <reloxx@interia.pl>
2021-04-10 12:52:24 +0200

    disance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1074,7 +1074,7 @@
  */
 
 // arms https://www.thingiverse.com/thing:3432489x
-#define NOZZLE_TO_PROBE_OFFSET { -27, -2, -1.68 }
+#define NOZZLE_TO_PROBE_OFFSET { -27, -2, -1.88 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
