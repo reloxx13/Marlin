a35523174d36077663671f05a6eaa4ed1d1bc662
reloxx13 <reloxx@interia.pl>
2022-01-05 18:49:38 +0100

    version

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -143,7 +143,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 v1.24.0"
+#define CUSTOM_MACHINE_NAME "Ender-3 v1.24.2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
